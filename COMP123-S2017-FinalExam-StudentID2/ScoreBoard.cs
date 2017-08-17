648f01b986a698c0d623b11742031a5f4189f44c
300930741 <300930741@PR-A315-15>
2017-08-17 11:34:37 -0400

    Created PRIVATE INSTANCE VARIABLES

--- COMP123-S2017-FinalExam-StudentID2/ScoreBoard.cs
+++ COMP123-S2017-FinalExam-StudentID2/ScoreBoard.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
+/*
+ * Name: Robin Singh
+ * Date: August 17,2017
+ * StudentID: 300930741
+ * Description: This is the ScoreBoard Class
+ * Version:0.1 : Created PRIVATE INSTANCE VARIABLES
+ */
 namespace COMP123_S2017_FinalExam_StudentID2
 {
+
     class ScoreBoard
     {
+        //PRIVATE PROPERTY (INSTANCE VARIABLES)
+        private TextBox _finalScoreTextBox;
+
+        private int _score;
+
+        private TextBox _scoreTextBox;
+
+        private int _time;
+
+        private TextBox _timeTextBox;
+
     }
+
+  
+    }
+
+
+
 }
