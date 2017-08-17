6803e2b3020d749311bd10bd584fad7933f6ba67
300930741 <300930741@PR-A315-15>
2017-08-17 11:57:40 -0400

    Created PUBLIC PROPERTIES

--- COMP123-S2017-FinalExam-StudentID2/ScoreBoard.cs
+++ COMP123-S2017-FinalExam-StudentID2/ScoreBoard.cs
@@ -10,29 +10,85 @@ using System.Windows.Forms;
  * Date: August 17,2017
  * StudentID: 300930741
  * Description: This is the ScoreBoard Class
- * Version:0.1 : Created PRIVATE INSTANCE VARIABLES
+ * Version:0.2 : Created PUBLIC PROPERTIES 
  */
 namespace COMP123_S2017_FinalExam_StudentID2
 {
 
     class ScoreBoard
     {
-        //PRIVATE PROPERTY (INSTANCE VARIABLES)
+        //PRIVATE PROPERTIES (INSTANCE VARIABLES)
         private TextBox _finalScoreTextBox;
-
         private int _score;
-
         private TextBox _scoreTextBox;
-
         private int _time;
-
         private TextBox _timeTextBox;
 
+
+        //PUBLIC PROPERTIES...
+
+        public TextBox FinalScoreTextBox
+        {
+            get
+            {
+                return this._finalScoreTextBox;
+            }
+            set
+            {
+                this._finalScoreTextBox = value;
+            }
+        }
+
+        public int Score
+        {
+            get
+            {
+                return this._score;
+            }
+            set
+            {
+                this._score = value;
+                
+            }
+        }
+        public TextBox ScoreTextBox
+        {
+            get
+            {
+                return this._scoreTextBox;
+            }
+            set
+            {
+                this._scoreTextBox = value;
+            }
+        }
+
+        public int Time
+        {
+            get
+            {
+                return this._time;
+            }
+            set
+            {
+                this._time = value;
+                
+            }
+        }
+
+        public TextBox TimeTextBox
+        {
+            get
+            {
+                return this._timeTextBox;
+            }
+            set
+            {
+                this._timeTextBox = value;
+            }
+        }
+
     }
-
-  
-    }
-
-
+     
 
 }
